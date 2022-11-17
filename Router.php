0d05d8a9be828cc7d4a17c5aa3871bbf585d26f7
jschaparro <ingenierojonathanchaparro@gmail.comm>
2022-11-16 20:09:57 -0500

    Deplyment de la app

--- Router.php
+++ Router.php
@@ -28,42 +28,23 @@ class Router
 
         // $auth = $_SESSION['login'] ?? null;
 
-        //solucion curso
-        //$currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-        
-        //Primera solucion
-        //$currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
 
-        //segunda solucion
-        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
-        
-        
-        $method = $_SERVER['REQUEST_METHOD'];
-            
-        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
-        $splitURL = explode('?', $currentUrl);
-        // debuguear($splitURL);
-        
-        if ($method === 'GET') {
-            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+        if (isset($_SERVER['PATH_INFO'])) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         } else {
-        $fn = $this->postRoutes[$splitURL[0]] ?? null;
+            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         }
 
-        //Fin de segunda solucion
-
-
-        //-----Anulamos esto para segunda solucion
-
-        // $method = $_SERVER['REQUEST_METHOD'];
-
-        // if ($method === 'GET') {
-        //     $fn = $this->getRoutes[$currentUrl] ?? null;
-        // } else {
-        //     $fn = $this->postRoutes[$currentUrl] ?? null;
-        // }
-
-        //---Fin de anulacion
+ 
+        $method = $_SERVER['REQUEST_METHOD'];
+        
+        
+        if ($method === 'GET') {
+            $fn = $this->getRoutes[$currentUrl] ?? null;
+        } else {
+            $fn = $this->postRoutes[$currentUrl] ?? null;
+        }
+       
 
 
         if ( $fn ) {
